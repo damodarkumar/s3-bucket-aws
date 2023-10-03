c969e144a39e1388996583e9e5917f58cecc46c9
damodarkumar <dhamu1010@gmail.com>
2023-10-03 14:45:22 +0200

    new project

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 # use ubuntu 20 AMI for EC2 instance
 
 provider "aws" {
-  region  = ""
+  region  = "eu-north-1"
 }
 
 resource "aws_s3_bucket" "platformengineering_bucket" {
   acl    = "private"
-}
 
   tags = {
     Name = var.bucketname
   }
+}
